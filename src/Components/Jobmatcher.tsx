7c85ee90f6781e2bc6c275b45c11e7a9859763a4
Deepan <deepan@example.com>
2026-02-17 09:56:03 +0530

    Fix case sensitivity for JobMatcher and GeminiService

--- src/Components/Jobmatcher.tsx
+++ src/Components/Jobmatcher.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
 import { Student, MatchResult } from '../types';
-import { matchJobToStudents } from '../geminiService';
+import { matchJobToStudents } from '../GeminiService';
 
 interface JobMatcherProps {
   students: Student[];
